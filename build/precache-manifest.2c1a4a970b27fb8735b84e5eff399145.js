e1828a8c2e8388f5e5164549d203fd4fc882529d
Max Chu <maxmchu@gmail.com>
2019-04-26 11:53:35 -0400

    Import

---
+++ build/precache-manifest.2c1a4a970b27fb8735b84e5eff399145.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7418e67e8255e94327b1511ea80db636",
+    "url": "/carriage-gh-pages/index.html"
+  },
+  {
+    "revision": "34d9dfa0888f09c607c5",
+    "url": "/carriage-gh-pages/static/css/main.3e545749.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/carriage-gh-pages/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "34d9dfa0888f09c607c5",
+    "url": "/carriage-gh-pages/static/js/main.66247983.chunk.js"
+  },
+  {
+    "revision": "08949ea442afa25305c5",
+    "url": "/carriage-gh-pages/static/js/runtime~main.ba37d991.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/carriage-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.scss';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Carriage from './Carriage';
+
+
+class App extends Component {
+
+  public render() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Carriage} />
+            <Route component={Carriage} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
